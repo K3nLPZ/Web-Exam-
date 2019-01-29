41dcd458eb7ca94ffc86014dff872b67399d5a4a
Kenia Lopez <U91603@ust-global.com>
2019-01-29 14:57:32 -0600

    changed

--- main.js
+++ main.js
@@ -1,26 +1,3 @@
-$('.visibility-cart').on('click',function(){
-       
-    var $btn =  $(this);
-    var $cart = $('.cart');
-    console.log($btn);
-    
-    if ($btn.hasClass('is-open')) {
-       $btn.removeClass('is-open');
-       $btn.text('O')
-       $cart.removeClass('is-open');     
-       $cart.addClass('is-closed');
-       $btn.addClass('is-closed');
-    } else {
-       $btn.addClass('is-open');
-       $btn.text('X')
-       $cart.addClass('is-open');     
-       $cart.removeClass('is-closed');
-       $btn.removeClass('is-closed');
-    }
-  
-                    
-  });
-  
       // SHOPPING CART PLUS OR MINUS
       $('a.qty-minus').on('click', function(e) {
           e.preventDefault();
@@ -66,3 +43,5 @@ $('.visibility-cart').on('click',function(){
               input.val(100);
           }
   });
+
+  
